b35f4a50cd1a664e1c3fa93a56dab5c8747a6697
Dorian Bayart <dbayart@users.noreply.github.com>
2016-04-27 01:54:21 +0200

    Update tabs_gallery.js

--- js/tabs_gallery.js
+++ js/tabs_gallery.js
@@ -2,11 +2,14 @@
 
 function myTabsActivation(  ) {
 	$('#myTabs a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
+    e.preventDefault();
+    $(this).tab('show');
+		
+		loadTab( $(this) );
+		
   });
+	$('#myTabs > li').first().addClass('active');
+	$('div.tab-content > div').first().addClass('in active');
+	loadTab( $('#myTabs > li').first() );
 }
-
-
-
 
