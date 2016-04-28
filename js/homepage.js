9b8fd029d4f3a4be7c33eb0dda4060488f6990ab
Dorian Bayart <dbayart@users.noreply.github.com>
2016-04-28 16:34:51 +0200

    Create homepage.js

---
+++ js/homepage.js
@@ -0,0 +1,7 @@
+
+
+$('.panel').hover(
+       function(){ $(this).addClass('panel-info').removeClass('panel-default') },
+       function(){ $(this).addClass('panel-default').removeClass('panel-info') }
+)
+
