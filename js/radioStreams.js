b5aa340b0f14e07b35ad9c3f17025062e4967d78
Dorian Bayart <dorianbayart@users.noreply.github.com>
2017-11-13 17:54:24 +0100

    Update radioStreams.js

--- js/radioStreams.js
+++ js/radioStreams.js
@@ -68,7 +68,7 @@ function generateList() {
 		$(this).addClass('panel panel-info');
 		
 		title.wrap('<div class="panel-heading" data-toggle="collapse" data-target="#'+uniqID+'" aria-expanded="false" aria-controls="'+uniqID+'">');
-		$(this).find('> #country, > #url, > #flag, > #slogan, > .tags').wrapAll('<div class="panel-body collapse" id="'+uniqID+'">').wrapAll('<div class="row">');
+		$(this).find('> #country, > #url, > #flag, > #slogan, > .tags').wrapAll('<div class="panel-collapse collapse" id="'+uniqID+'">').wrapAll('<div class="panel-body">').wrapAll('<div class="row">');
 		
 		$(this).find('#country, #flag, .tags').wrapAll('<div class="col-sm-6">');
 		$(this).find('#url, #slogan').wrapAll('<div class="col-sm-6">');
