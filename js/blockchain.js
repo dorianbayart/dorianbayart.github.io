1b0cee55a41e993ed13650fb34de346fa421011e
Dorian Bayart <dorianbayart@users.noreply.github.com>
2018-03-10 00:17:27 +0100

    Update blockchain.js

--- js/blockchain.js
+++ js/blockchain.js
@@ -32,39 +32,41 @@ function filtering(json, criteria){
 }
 
 function generateList(data) {
-
-
-	//var jsonParsed = JSON.parse(data);
-  var jsonParsed = data;
-
+	var jsonParsed = data;
 	var text = $(readJson( sortStreams(jsonParsed, "rank", true) ));
   $( '#json' ).append( data );
 	$( '#json_ul' ).html("").append( text );
 
 	$('#json_ul > div').each(function() {
 
-    $(this).find('> #id, > #24h_volume_usd, > #market_cap_usd, > #available_supply, > #total_supply, > #max_supply, > #percent_change_7d, > #last_updated, > #24h_volume_eur, > #market_cap_eur').hide();
+		$(this).addClass('row');
+    $(this).find('span').hide();
 
-		var title = $(this).children( 'span#name' );
-		var country = $(this).children( 'span#country' );
-		var flag = $(this).children( 'span#flag' );
-		var url = $(this).children( 'span#url' );
-		var slogan = $(this).children( 'span#slogan' );
+		var id = $(this).children( 'span#id' ).text();
+		var name = $(this).children( 'span#name' ).text();
+		var symbol = $(this).children( 'span#symbol' ).text();
+		var rank = $(this).children( 'span#rank' ).text();
 
-		var uniqID = $(this).children( 'span#id' ).text();
+		var price_eur = $(this).children( 'span#price_eur' ).text();
+		var price_usd = $(this).children( 'span#price_usd' ).text();
 
-		$(this).addClass('panel panel-info');
+		var percent_change_1h = $(this).children( 'span#percent_change_1h' ).text();
+		var percent_change_24h = $(this).children( 'span#percent_change_24h' ).text();
+		var percent_change_7d = $(this).children( 'span#percent_change_7d' ).text();
 
-		//title.wrap('<div class="panel-heading" data-toggle="collapse" data-target="#'+uniqID+'" aria-expanded="false" aria-controls="'+uniqID+'">');
-    $(this).find('> #rank, > #name, > #symbol, > #price_eur').wrapAll('<div class="panel-heading" data-toggle="collapse" data-target="#'+uniqID+'" aria-expanded="false" aria-controls="'+uniqID+'">').wrapAll('<div class="row">');
-		$(this).find('> #price_usd, > #price_btc, > #percent_change_1h, > #percent_change_24h').wrapAll('<div class="panel-collapse collapse" id="'+uniqID+'">').wrapAll('<div class="panel-body">').wrapAll('<div class="row">');
+		$(this).find('span').remove();
 
-		$(this).find('#rank, #name, #symbol, #price_eur').each(function() {
-      $(this).wrap('<div class="col-xs-3">');
-    });
-    $(this).find('#price_usd, #price_btc, #percent_change_1h, #percent_change_24h').each(function() {
-      $(this).wrap('<div class="col-xs-3">');
-    });
+		var text = "";
+		text += "<div class='col-xs-1'><span id='rank'>" + rank + "</span></div>";
+		text += "<div class='col-xs-5'><span id='name'>" + name + " (" + symbol + ")</span></div>";
+		text += "<div class='col-xs-2'><span id='price_eur'> " + price_eur + "</span></div>";
+		text += "<div class='col-xs-4'><div class='row'>";
+			text += "<div class='col-xs-4'><span id='percent_change_1h'>" + percent_change_1h + "</span></div>";
+			text += "<div class='col-xs-4'><span id='percent_change_24h'>" + percent_change_24h + "</span></div>";
+			text += "<div class='col-xs-4'><span id='percent_change_7d'>" + percent_change_7d + "</span></div>";
+		text += "</div></div>";
+
+		$(this).append(text);
 
     // Montants
     var euros = $(this).find('#price_eur').text();
@@ -79,27 +81,8 @@ function generateList(data) {
     $(this).find('#price_eur').append('€');
     $(this).find('#price_usd').prepend('$');
     $(this).find('#price_btc').append(' BTC');
-    $(this).find('#percent_change_1h').append('% (1h)');
-    $(this).find('#percent_change_24h').append('% (24h)');
 
-		/*slogan.wrap('<blockquote class="blockquote">');
-
-		$('#'+uniqID).on('show.bs.collapse', function () { // show the audio tag
-			if(url.text().toLowerCase().includes('http'))
-				url.html( '<audio controls preload="metadata"><source src="' + url.text().toLowerCase() + '">Your browser does not support the audio element.</audio>' );
-		})
-		$('#'+uniqID).on('hidden.bs.collapse', function () { // destroy the audio tag
-
-		});
-
-    */
 	});
-
-	/*$( '.streams #flag' ).each(function() {
-		var codeISO = $(this).text();
-		$(this).html('<figure><img src="https://lipis.github.io/flag-icon-css/flags/4x3/'+ codeISO +'.svg" /></figure>');
-	});
-	$('.tags > *').addClass( 'label label-info' );*/
 }
 
 $(document).ready(function() {
