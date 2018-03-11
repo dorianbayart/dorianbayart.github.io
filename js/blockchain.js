163cf8c92a5ff3069f25cbf7b8dc668315a68cf5
Dorian Bayart <dorianbayart@users.noreply.github.com>
2018-03-11 18:20:27 +0100

    Update blockchain.js

--- js/blockchain.js
+++ js/blockchain.js
@@ -39,9 +39,6 @@ function generateList(data) {
 
 	$('#json_ul > div').each(function() {
 
-		$(this).addClass('row');
-    $(this).find('span').hide();
-
 		var id = $(this).children( 'span#id' ).text();
 		var name = $(this).children( 'span#name' ).text();
 		var symbol = $(this).children( 'span#symbol' ).text();
@@ -54,13 +51,16 @@ function generateList(data) {
 		var percent_change_24h = $(this).children( 'span#percent_change_24h' ).text();
 		var percent_change_7d = $(this).children( 'span#percent_change_7d' ).text();
 
+		$(this).addClass('row');
+		$(this).attr('id', id);
 		$(this).find('span').remove();
 
 		var text = "";
-		text += "<div class='col-xs-1'><span id='rank'>" + rank + "</span></div>";
-		text += "<div class='col-xs-5'><span id='name'>" + name + " (" + symbol + ")</span></div>";
-		text += "<div class='col-xs-2'><span id='price_eur'> " + price_eur + "</span></div>";
-		text += "<div class='col-xs-4'><div class='row'>";
+		text += "<div class='col-xs-2 col-sm-1'><span id='rank'>" + rank + "</span></div>";
+		text += "<div class='hidden-xs col-sm-1'><span id='symbol'>" + symbol + "</span></div>";
+		text += "<div class='col-xs-6 col-sm-4'><span id='name'>" + name + "</span></div>";
+		text += "<div class='col-xs-4 col-sm-2'><span id='price_eur'> " + price_eur + "</span></div>";
+		text += "<div class='hidden-xs col-sm-4'><div class='row'>";
 			text += "<div class='col-xs-4'><span id='percent_change_1h'>" + percent_change_1h + "</span></div>";
 			text += "<div class='col-xs-4'><span id='percent_change_24h'>" + percent_change_24h + "</span></div>";
 			text += "<div class='col-xs-4'><span id='percent_change_7d'>" + percent_change_7d + "</span></div>";
@@ -68,25 +68,49 @@ function generateList(data) {
 
 		$(this).append(text);
 
-    // Montants
-    var euros = $(this).find('#price_eur').text();
-    if(euros > 1) {
-      euros = Math.round(euros * 100) / 100;
-    } else {
-      euros = Math.round(euros * 10000) / 10000;
-    }
-    $(this).find('#price_eur').text(euros);
-
-    $(this).find('#rank').prepend('#');
-    $(this).find('#price_eur').append('€');
-    $(this).find('#price_usd').prepend('$');
-    $(this).find('#price_btc').append(' BTC');
+		updateSymbols($(this));
 
 	});
 }
 
-$(document).ready(function() {
+function updateSymbols(doc) {
+	var euros = $(doc).find('#price_eur').text();
+	if(euros > 1) {
+		euros = Math.round(euros * 100) / 100;
+	} else {
+		euros = Math.round(euros * 10000) / 10000;
+	}
+	$(doc).find('#price_eur').text(euros);
 
+	$(doc).find('#rank').prepend('#');
+	$(doc).find('#price_eur').append('€');
+	$(doc).find('#price_usd').prepend('$');
+	$(doc).find('#price_btc').append(' BTC');
+}
+
+function updateList() {
+
+	$.ajax({
+    url: "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=25",
+    dataType: 'json',
+    success: function(data) {
+			$.each( data, function( number, line ) {
+				var id = line.id;
+				$.each( line, function( key, val ) {
+					$("#"+id+" #"+key).text(val);
+				});
+				updateSymbols($("#"+id));
+			});
+
+    },
+    error: function(e) {
+      alert(e);
+    }
+  });
+
+}
+
+$(document).ready(function() {
   $.ajax({
     url: "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=25",
     dataType: 'json',
@@ -98,7 +122,7 @@ $(document).ready(function() {
     }
   });
 
-		//generateList();
+	var updateTimer = setInterval(function(){ updateList() }, 15000);
 
 		$( '#filterSelectLanguage' ).on("change", function() {
 			generateList();
